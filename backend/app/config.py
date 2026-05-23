c0be57e0906b925df3b735a0a2aaf45327c14cb2
Zack <168221270+Zval321@users.noreply.github.com>
2026-05-23 09:32:30 -0500

    finished up the utils auth section for the application

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,20 +1,16 @@
 from pydantic_settings import BaseSettings
 
-CORS_ORIGINS = [
-  "http://localhost:5173", 
-   "http://localhost:5174"
-]
+CORS_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
 
 
-class Settings(BaseSettings): 
-  DATABASE_URL: str
-  SECRET_KEY: str
-  ALGORITHM: str = "HS256"
-  
-  class Config: 
-    env_file = ".env"
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCES_TOKEN_EXPIRE_MINUTES = int = 1440
+
+    class Config:
+        env_file = ".env"
+
 
 settings = Settings()
-
-
-
