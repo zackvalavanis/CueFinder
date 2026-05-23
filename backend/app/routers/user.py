b7275011e5be72083f452ae84271a67f07584ebf
Zack <168221270+Zval321@users.noreply.github.com>
2026-05-23 09:41:43 -0500

    current user and auth

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -2,6 +2,7 @@ from app.schemas.users import UserResponse, UserCreate
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app.models.user import User
+from app.utils.auth import get_current_user
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from uuid import UUID
@@ -14,3 +15,8 @@ router = APIRouter()
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
+
+
+@router.get("users/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -21,3 +21,13 @@ class UserResponse(UserBase):
 
 class UserCreate(UserBase):
     password: str
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
