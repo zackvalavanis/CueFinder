f9562c92e7b6a1a0d9e092188715e288c3fa4726
Zack <168221270+Zval321@users.noreply.github.com>
2026-05-23 08:29:46 -0500

    config file, database file and the main file completed

---
+++ backend/app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import sessionmaker, declarative_base 
+from sqlalchemy import create_engine
+from app.config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db: 
+  db=SessionLocal()
+  try: 
+    yield db
+  finally: 
+    db.close()
---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI  
+from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+from app.config import CORS_ORIGINS
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+  CORSMiddleware, 
+  allow_origins=CORS_ORIGINS, 
+  allow_credentials=True, 
+  allow_methods=["*"], 
+  allow_headers=["*"]
+)
