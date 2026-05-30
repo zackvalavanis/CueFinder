b483f80dfc5be3417a7ab87d935bd7e121768a88
Zack <168221270+Zval321@users.noreply.github.com>
2026-05-30 15:46:12 -0500

    tests are all passing for user and p_tables

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app.database import get_db
 
 client = TestClient(app)
 
@@ -8,3 +9,24 @@ def test_get_users():
     response = client.get("/users")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
+
+def test_create_user():
+    new_user_response = client.post(
+        "/users",
+        json={
+            "first_name": "Steve",
+            "last_name": "Rodgers",
+            "email": "Steve@email.com",
+            "password": "password123",
+        },
+    )
+
+    assert new_user_response.status_code == 200
+    assert new_user_response.json()["email"] == "Steve@email.com"
+
+    from app.models.user import User
+
+    db = next(get_db())
+    db.query(User).filter(User.email == "Steve@email.com").delete()
+    db.commit()
