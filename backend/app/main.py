10f8624db00675d3024fc99370521d82049cf640
Zack <168221270+Zval321@users.noreply.github.com>
2026-06-28 20:07:58 -0500

    updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,8 +20,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 app.include_router(user_router)
